f37566f9e1967eaa16d2c108e50f63ff96951e46
Mike Byrne <mike@thirteentwelve.com>
2015-04-07 11:06:14 +0100

    Initial code commit - WIP

---
+++ README.md
@@ -0,0 +1,160 @@
+# min.js
+
+v2
+
+A super tiny JavaScript library to execute simple DOM querying, hooking name spaced event listeners, trigger events and some simple DOM node helpers. Creates objects with prototypes rather than adding prototypes to nodes themselves.
+
+This is a re-write of my [fork](https://github.com/13twelve/min_v1.js) of Remy Sharp's [min.js](https://github.com/remy/min.js).
+
+Checks for querySelectorAll and addEventListener and returns undefined if not present.
+
+**WIP** Needs testing!
+
+## Filesize
+
+* 5kb uncompressed
+* 2kb minified
+* 1kb minified and gzipped
+
+## Query elements
+
+```js
+var divs = $('div'); // minjs object
+```
+
+Creates minjs object: an array of nodes with prototype methods.
+
+Optionally you can supply a context in which to look:
+
+```js
+var divs = $('div','#content'); // minjs object
+```
+
+You can also pass in a node, to turn it into a minjs object:
+
+```js
+var node = document.getElementById("#content"); // #content node
+var container = $(node); // minjs object
+```
+
+To return nodes from the minjs object:
+
+```js
+var divs = $('div','#content');
+var first_div = divs[0]; // node
+```
+
+
+## Events
+
+### Bind events with on()
+
+Basic:
+
+```js
+$('p:first-child a').on('click', function (event) {
+  event.preventDefault();
+  // do something else
+});
+```
+
+Or with a namespace:
+```js
+$('p:first-child a').on('click.foo', function (event) {
+  event.preventDefault();
+  // do something else
+});
+```
+
+Note:
+* only accepts singular events and singular namespaces
+* **must** contain an event type (namespace is optional)
+
+### Unbind events with off()
+
+```js
+$('p:first-child a').off(); // clears all handlers
+$('p:first-child a').off('click'); // clears just the click handlers
+$('p:first-child a').off('click.foo'); // clears just foo namespaced click handlers
+$('p:first-child a').off('.foo'); // clears foo namespaced handlers
+```
+
+### Custom events
+
+```js
+$(document).on('foo', function () {
+  // foo was fired
+});
+
+$(document).trigger('foo');
+```
+
+## Looping
+
+```js
+$('p').each(function(el,index) {
+  console.log(el.innerHTML);
+});
+```
+
+To break a loop, return false:
+
+```js
+// assume you have 5 p's
+$('p').each(function(el,index) {
+  console.log(i);
+  if (i === 1) {
+    return false;
+  }
+});
+// 0, 1
+```
+
+## Chaining events
+
+```js
+$('a').on('foo', bar).on('click', doclick).trigger('foobar');
+```
+
+## Add, remove, has CSS class
+
+```js
+$('a').addClass("foo"); // adds class to all links
+$('a').removeClass("bar"); // removes class from all links
+var is_foo = $('a').hasClass("foo"); // assumes the first item, returns true/false
+```
+
+## Read/write attributes
+
+```js
+$('div:last-child').attr('data-url'); // returns contents of attribute
+$('div:first-child').attr('data-url','http://www.github.com/13twelve'); // sets attribute
+```
+
+Assumes the first item if passed a collection larger than 1.
+
+## Read/write CSS styles
+
+```js
+$('a:last-child').css('color'); // returns computed value of color
+$('a:first-child').css('color','#000000'); // sets style
+$('a:first-child').css({
+  color: '#000000',
+  paddingRight: '10px'
+}); // sets multiple styles
+```
+
+Assumes the first item if passed a collection larger than 1.
+
+## Silent failing
+
+This tiny library silently fails when it doesn't match any elements.
+
+## To Do
+
+* fix IE9 add class method
+* add index method, to return index of node in collection
+
+## More info
+
+* License: MIT
---
+++ src/min.js
@@ -0,0 +1,174 @@
+$ = (function () {
+  console.warn("minjs v2.0.0 - in development - methods may not work as expected");
+
+  // Kill exeuction for bad browsers
+  if(typeof document.querySelectorAll !== undefined && !('addEventListener' in window)) {
+    return;
+  }
+
+  function minjs(elements) {
+    if (elements) {
+      this.length = elements.length;
+      for (var i = 0; i < this.length; i++) {
+        this[i] = elements[i];
+      }
+    }
+  }
+
+  // a counter for unique event ids - used for namespacing event handlers
+  var event_uuid = 0;
+  // a store for events - used for namespacing event handlers
+  var events_cache = {};
+
+  function each(arr,func) {
+    //Array.prototype.forEach.call(arr, func);
+    var arr_length = arr.length;
+    for (var i = 0; i < arr_length; i++) {
+      var value = func.call(arr[i],arr[i],i);
+      if (value === false) {
+        break;
+      }
+    }
+    return arr;
+  }
+
+  minjs.prototype = {
+    each:function(func){
+      return each(this,func);
+    },
+    on:function(type,fn) {
+      each(this,function(el,i){
+        event_uuid++;
+        if (!el.handlers) {
+          el.handlers = {};
+        }
+        // check for namespace
+        var type_arr = type.split(".");
+        // store event data
+        el.handlers[event_uuid] = type;
+        events_cache[event_uuid] = {
+          type: type_arr[0],
+          namespace: type_arr[1] || "",
+          fn: fn
+        };
+        // add listener
+        el.addEventListener(type_arr[0], fn, false);
+      });
+      // allow for chaining
+      return this;
+    },
+    off:function(nodelist,type) {
+      each(this,function(el,i){
+        // check for namespace
+        var node = el;
+        var node_handlers = node.handlers || [];
+        var type_arr = (typeof type === "undefined") ? [] : type.split(".");
+        var event_type, event_namespace;
+        //
+        if (type_arr.length > 0) {
+          event_type = type_arr[0] || "";
+          event_namespace = type_arr[1] || "";
+        }
+        // loop handlers
+        Object.keys(node_handlers).forEach(function(key,i){
+          if (
+            (type_arr.length === 0) || // off(); so remove all events from node
+            (event_type === events_cache[key].type && event_namespace === events_cache[key].namespace) || // match type and namespace
+            (event_type === events_cache[key].type && event_namespace === "") || // match type and no namespace
+             (event_namespace === events_cache[key].namespace && event_type === "") // match namespace and no type
+          ){
+            // remove the listener
+            node.removeEventListener(events_cache[key].type, events_cache[key].fn, false);
+            // clean up after yourself
+            delete node.handlers[key];
+            delete events_cache[key];
+          }
+        });
+      });
+      // allow for chaining
+      return this;
+    },
+    trigger:function(type, data) {
+      // construct an HTML event. This could have
+      // been a real custom event
+      var event = document.createEvent('HTMLEvents');
+      event.initEvent(type, true, true);
+      event.data = data || {};
+      event.eventName = type;
+      //
+      each(this,function(el,i){
+        event.target = el;
+        el.dispatchEvent(event);
+      });
+      // allow for chaining
+      return this;
+    },
+    addClass:function(className){
+      each(this,function(el,i){
+        if (el.classList) {
+          el.classList.add(className);
+        } else {
+          // TODO: check if has class first
+          el.className += ' ' + className;
+        }
+      });
+      return this;
+    },
+    removeClass:function(className){
+      each(this,function(el,i){
+        if (el.classList) {
+          el.classList.remove(className);
+        } else {
+          el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+      });
+      return this;
+    },
+    hasClass:function(className){
+      var el = (this.length > 0) ? this[0] : this;
+      if (el.classList) {
+        return el.classList.contains(className);
+      } else {
+        return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+      }
+      return el;
+    },
+    attr:function(){
+      var el = (this.length > 0) ? this[0] : this;
+      if (v === undefined) {
+        return el.getAttribute(a);
+      } else {
+        el.setAttribute(a,v);
+        return el;
+      }
+    },
+    css:function(p,v) {
+      var el = (this.length > 0) ? this[0] : this;
+      if (typeof p === "object") {
+        for (var n in p) {
+          el.style[n] = p[n];
+        }
+        return el;
+      } else {
+        if (v === undefined) {
+          return document.defaultView.getComputedStyle(el,null).getPropertyValue(p);
+        } else {
+          el.style[p] = v;
+          return el;
+        }
+      }
+    }
+  }
+
+  function $(selector,context){
+    var nodes = [];
+    if (selector && typeof selector !== "string") {
+      nodes = (selector.addClass) ? selector : [selector];
+    } else if (selector) {
+      nodes = (context || document).querySelectorAll(selector || '☺');
+    }
+    return new minjs(nodes);
+  }
+
+  return $;
+})();
